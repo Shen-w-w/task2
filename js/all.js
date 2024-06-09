9f62ecca6b10cfc33605777fcabe707d61165754
Shen-w-w <hatty1376@gmail.com>
2024-06-10 01:48:44 +0800

    Update all.js
    upload

--- js/all.js
+++ js/all.js
@@ -8,3 +8,28 @@ $('.navbar-btn').click(function() {
       //利用active切換list顯示狀態
     $('.list').toggleClass('active');
   })
+
+
+//swiper套件
+
+  const swiper = new Swiper(".swiper", {
+  
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    grid: {
+      rows: 3,
+    },
+    breakpoints: {
+      768: {      
+        grid: {
+          rows: 1,
+        },
+        slidesPerView: 3,
+        spaceBetween: 24,
+      }
+    }
+  });
+  
+  
